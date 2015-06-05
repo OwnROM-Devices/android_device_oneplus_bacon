dd8c416b2dc9beb8adb372d4f69c39b1f9c1e3f4
Akhil Narang <akhilnarang.1999@gmail.com>
2015-06-05 17:07:41 +0530

    Ownified device tree
    Change-Id: If78953af91a4846e326ecd2cf2401d5c7a24aa5c
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo cm_bacon-user
-add_lunch_combo cm_bacon-userdebug
-add_lunch_combo cm_bacon-eng
+add_lunch_combo ownrom_bacon-user
+add_lunch_combo ownrom_bacon-userdebug
+add_lunch_combo ownrom_bacon-eng
